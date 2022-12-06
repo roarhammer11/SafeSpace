d605ba076c41b5f9a22a1de0fcea9da154f9ba17
Anthony <roarhammer11@gmail.com>
2022-12-06 16:38:58 +0800

    converted class components to function components

--- src/components/dashboardComponent.js
+++ src/components/dashboardComponent.js
@@ -1,33 +1,13 @@
-import React, {Component} from "react";
 import Button from "./non-route-components/buttonComponent";
-class Page2 extends Component {
-  constructor() {
-    super();
-    this.nextPage = this.nextPage.bind(this);
-    this.state = {
-      sampleString: "Page 2",
-    };
-  }
 
-  nextPage() {
-    window.location.replace("/");
-  }
+const Dashboard = () => {
+  return (
+    <div>
+      <center>
+        <Button name="To Index" location="/" />
+      </center>
+    </div>
+  );
+};
 
-  componentDidMount() {}
-
-  componentWillUnmount() {}
-
-  render() {
-    return (
-      <div>
-        <center>
-          <h1>{this.state.sampleString}</h1>
-          <Button />
-          <button onClick={this.nextPage}> To index </button>
-        </center>
-      </div>
-    );
-  }
-}
-
-export default Page2;
+export default Dashboard;
--- src/components/indexComponent.js
+++ src/components/indexComponent.js
@@ -1,47 +1,41 @@
-import React, {Component} from "react";
 import Button from "./non-route-components/buttonComponent";
-class Index extends Component {
-  componentDidMount() {}
 
-  componentWillUnmount() {}
-
-  render() {
-    return (
-      <div className="App-Container container-fluid h-100 d-flex">
-        <div className="left w-50 p-3 ms-5 h-100">
-          <div className=" d-flex justify-content-center">
-            <p>
-              Bringing mental health professionals from all over the world,
-              SafeSpace serves as an easy-to-cross bridge linking them and those
-              who need their professional help. By "easy-to-cross" bridge, we
-              mean that this website provides easy access to professional help
-              for all sorts of mental disorders. You are not alone, and the most
-              importantly, you are getting help :)
-              <br />
-              <br />
-              Suicide hotline in your area is 0966-351-4518
-            </p>
-          </div>
+const Index = () => {
+  return (
+    <div className="App-Container container-fluid h-100 d-flex">
+      <div className="left w-50 p-3 ms-5 h-100">
+        <div className=" d-flex justify-content-center">
+          <p>
+            Bringing mental health professionals from all over the world,
+            SafeSpace serves as an easy-to-cross bridge linking them and those
+            who need their professional help. By "easy-to-cross" bridge, we mean
+            that this website provides easy access to professional help for all
+            sorts of mental disorders. You are not alone, and the most
+            importantly, you are getting help :)
+            <br />
+            <br />
+            Suicide hotline in your area is 0966-351-4518
+          </p>
         </div>
-        <div className="right w-50">
-          <div className="d-flex flex-column align-items-center">
-            <h1>
-              SafeSpace
-              <br />
-            </h1>
+      </div>
+      <div className="right w-50">
+        <div className="d-flex flex-column align-items-center">
+          <h1>
+            SafeSpace
+            <br />
+          </h1>
 
-            <h5>a judement free place</h5>
-            <div className="sign-up-button w-25">
-              <Button name="Sign up" location="/Dashboard" />
-            </div>
-            <div className="w-25">
-              <Button name="Log in" location="/Dashboard" />
-            </div>
+          <h5>a judement free place</h5>
+          <div className="sign-up-button w-25">
+            <Button name="Sign up" location="/Dashboard" />
+          </div>
+          <div className="w-25">
+            <Button name="Log in" location="/Dashboard" />
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Index;
