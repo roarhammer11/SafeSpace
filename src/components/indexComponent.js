101c328d9124c5f986e839ee1c964cfec9c0367d
Anthony <roarhammer11@gmail.com>
2022-12-06 15:36:27 +0800

    Added Landing Page

--- src/components/indexComponent.js
+++ src/components/indexComponent.js
@@ -1,4 +1,3 @@
-import logo from ".././logo.svg";
 import React, {Component} from "react";
 class Index extends Component {
   constructor() {
@@ -14,22 +13,46 @@ class Index extends Component {
   }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          <button onClick={this.nextPage}> To Page2 </button>
-        </header>
+      <div className="App-Container container-fluid h-100 d-flex">
+        <div className="left w-50 p-3 ms-5 h-100">
+          <div className=" d-flex justify-content-center">
+            <p>
+              Bringing mental health professionals from all over the world,
+              SafeSpace serves as an easy-to-cross bridge linking them and those
+              who need their professional help. By "easy-to-cross" bridge, we
+              mean that this website provides easy access to professional help
+              for all sorts of mental disorders. You are not alone, and the most
+              importantly, you are getting help :)
+              <br />
+              <br />
+              Suicide hotline in your area is 0966-351-4518
+            </p>
+          </div>
+        </div>
+        <div className="right w-50">
+          <div className="d-flex flex-column align-items-center">
+            <h1>
+              SafeSpace
+              <br />
+            </h1>
+
+            <h5>a judement free place</h5>
+            <button
+              type="button"
+              className="btn btn-primary btn-lg  w-25 sign-up-button"
+              onClick={this.nextPage}
+            >
+              Sign Up
+            </button>
+            <button
+              type="button"
+              className="btn btn-primary btn-lg  w-25"
+              onClick={this.nextPage}
+            >
+              Login
+            </button>
+          </div>
+        </div>
       </div>
     );
   }
