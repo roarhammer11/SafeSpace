3fa9d340e5bd2b873e450c4ce80506c56f38f143
Nene <ernestcurativo@gmail.com>
2022-12-08 16:16:12 +0800

    Added designs and login/signup form in indexComponents.js

--- src/components/indexComponent.js
+++ src/components/indexComponent.js
@@ -1,4 +1,5 @@
 import Button from "./non-route-components/buttonComponent";
+import "./css-components/usersForm.css";
 
 const Index = () => {
   return (
@@ -10,7 +11,7 @@ const Index = () => {
             SafeSpace serves as an easy-to-cross bridge linking them and those
             who need their professional help. By "easy-to-cross" bridge, we mean
             that this website provides easy access to professional help for all
-            sorts of mental disorders. You are not alone, and the most
+            sorts of mental disorders. You are not alone, and most
             importantly, you are getting help :)
             <br />
             <br />
@@ -25,12 +26,33 @@ const Index = () => {
             <br />
           </h1>
 
-          <h5>a judgment free place</h5>
-          <div className="sign-up-button w-25">
-            <Button name="Sign up" location="/Dashboard" />
-          </div>
-          <div className="w-25">
-            <Button name="Log in" location="/Dashboard" />
+          <h5 className="tagline">a judgment free place</h5>
+          <div className="main">  	
+            <input type="checkbox" id="chk" aria-hidden="true" />
+
+              <div class="signup">
+                <form>
+                  <label for="chk" aria-hidden="true">Sign up</label>
+                  <select>
+                    <option selected="true" disabled="disabled">Select type of user:</option>
+                    <option>Mental Health Professional</option>
+                    <option>Not a Mental Health Professional</option>
+                  </select>
+                  <input type="text" name="txt" placeholder="User name" required="" />
+                  <input type="email" name="email" placeholder="Email" required="" />
+                  <input type="password" name="pswd" placeholder="Password" required="" />
+                  <button>Sign up</button>
+                </form>
+              </div>
+
+              <div className="login">
+                <form>
+                  <label for="chk" aria-hidden="true">Login</label>
+                  <input type="email" name="email" placeholder="Email" required="" />
+                  <input type="password" name="pswd" placeholder="Password" required="" />
+                  <button>Login</button>
+                </form>
+              </div>
           </div>
         </div>
       </div>
