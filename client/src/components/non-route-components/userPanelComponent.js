ea5f9f4474751aba6dab8633c44b85d3298c4144
nene <ernestcurativo@gmail.com>
2022-12-21 23:15:40 +0800

    changed dashboardComponent.js

--- client/src/components/non-route-components/userPanelComponent.js
+++ client/src/components/non-route-components/userPanelComponent.js
@@ -1,35 +1,39 @@
 import Button from "./buttonComponent";
+import "../css-components/userPanel.css";
 const UserPanel = () => {
   return (
+    <div className="navright">
     <div className="App-Container container-fluid h-100 d-flex">
       <div className="w-100 p-3 h-100">
         <div className=" d-flex justify-content-around">
           <div className="d-flex flex-column mt-5">
-            <h4># Following</h4>
-            <h4># Followers</h4>
+            <h4>0 Following</h4>
+            <h4>0 Followers</h4>
           </div>
-          <img src="https://via.placeholder.com/150x150"></img>
+          <img src="https://via.placeholder.com/150x150" alt="sample"></img>
         </div>
         <center>
-          <p>Lorem ipsum dolor sit amet</p>
+          <p>Some features are not yet available...</p>
         </center>
         <div id="user-panel" className="d-flex flex-column mt-5">
           <button>Edit Profile</button>
           <Button
+            className="navbutton"
             name="Mental Health Professionals Near Me"
             location="/Dashboard/MentalHealthProfessionals"
           />
           <Button name="News Feed" location="/Dashboard" />
-          <Button name="Messages" location="/Dashboard/Messages" />
+          <Button className="navbutton" name="Messages" location="/Dashboard/Messages" />
           <Button
             name="Mental Disorders Filter"
             location="/Dashboard/MentalDisordersFilter"
           />
-          <Button name="Explore" location="/Dashboard/Explore" />
+          <Button className="navbutton" name="Explore" location="/Dashboard/Explore" />
           <button>Settings</button>
-          <Button name="Logout" location="/" />
+          <Button className="navbutton" name="Logout" location="/" />
         </div>
       </div>
+    </div>
     </div>
   );
 };
